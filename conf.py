9fa68d044f72b2b4ce670d04c90e601e78d4219b
Geoffrey Garrett <g.h.garrett13@gmail.com>
2021-05-03 21:04:40 +0200

    Updated concept

---
+++ conf.py
@@ -0,0 +1,164 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import ablog
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'tudat-space-blog'
+copyright = '2021, tudat-space'
+author = 'tudat-space'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.intersphinx',
+    'ablog',
+    'sphinx_panels',
+]
+
+# -- General ABlog Options ----------------------------------------------------
+
+# A path relative to the configuration directory for blog archive pages.
+# blog_path = 'blog'
+
+# The "title" for the blog, used in active pages.  Default is ``'Blog'``.
+blog_title = u'The ``tudat-space`` Blog'
+
+# Base URL for the website, required for generating feeds.
+# e.g. blog_baseurl = "http://example.com/"
+
+# Choose to archive only post titles. Archiving only titles can speed
+# up project building.
+# blog_archive_titles = False
+blog_languages = {
+    'en': ('English', None),
+}
+blog_authors = {
+    'Geoffrey': ('Geoffrey H. Garrett', 'https://github.com/ggarrett13'),
+    'Dominic': ('Dominic Dirkx', ''),
+}
+
+# -- ABlog Sidebars -------------------------------------------------------
+
+# There are seven sidebars you can include in your HTML output.
+# postcard.html provides information regarding the current post.
+# recentposts.html lists most recent five posts. Others provide
+# a link to a archive pages generated for each tag, category, and year.
+# In addition, there are authors.html, languages.html, and locations.html
+# sidebars that link to author and location archive pages.
+html_sidebars = {
+    '**': [
+        'postcard.html',
+        'recentposts.html', 'tagcloud.html',
+        'archives.html',
+        'searchbox.html',
+    ],
+}
+
+# -- CUSTOM: FontAwesome Inline Icons https://sphinx-panels.readthedocs.io/en/latest/#inline-icons
+html_css_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"]
+panels_add_fontawesome_latex = True
+
+# -- Blog Feed Options --------------------------------------------------------
+
+# Turn feeds by setting :confval:`blog_baseurl` configuration variable.
+# Choose to create feeds per author, location, tag, category, and year,
+# default is ``False``.
+# blog_feed_archives = False
+
+# Choose to display full text in blog feeds, default is ``False``.
+# blog_feed_fulltext = False
+
+# Blog feed subtitle, default is ``None``.
+# blog_feed_subtitle = None
+
+# Choose to feed only post titles, default is ``False``.
+# blog_feed_titles = False
+
+# Specify number of recent posts to include in feeds, default is ``None``
+# for all posts.
+# blog_feed_length = None
+
+# -- Font-Awesome Options -----------------------------------------------------
+
+# ABlog templates will use of Font Awesome icons if one of the following
+# is ``True``
+
+# Link to `Font Awesome`_ at `Bootstrap CDN`_ and use icons in sidebars
+# and post footers.  Default: ``False``
+fontawesome_link_cdn = "https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"
+
+# Sphinx_ theme already links to `Font Awesome`_.  Default: ``False``
+# fontawesome_included = False
+
+# Alternatively, you can provide the path to `Font Awesome`_ :file:`.css`
+# with the configuration option: fontawesome_css_file
+# Path to `Font Awesome`_ :file:`.css` (default is ``None``) that will
+# be linked to in HTML output by ABlog.
+# fontawesome_css_file = None
+
+# -- Disqus Integration -------------------------------------------------------
+
+# You can enable Disqus_ by setting ``disqus_shortname`` variable.
+# Disqus_ short name for the blog.
+# disqus_shortname = None
+
+# Choose to disqus pages that are not posts, default is ``False``.
+# disqus_pages = False
+
+# Choose to disqus posts that are drafts (without a published date),
+# default is ``False``.
+# disqus_drafts = False
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+
+# html_theme = 'alabaster'
+# html_theme = 'sphinx_book_theme'
+html_theme = "insipid"
+html_title = 'tudat-space'
+
+if html_theme=="insipid":
+    html_add_permalinks = '\N{SECTION SIGN}'
+    html_context = {'display_github': True, 'github_user': 'sphinx-themes',
+                    'github_repo': 'sphinx-themes.org',
+                    'conf_py_path': '/sample-docs/', 'commit': 'master'}
+    html_copy_source = False
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_logo = "_static/cover.png"
---
+++ index.rst
@@ -0,0 +1,13 @@
+the ``tudat-space`` blog
+========================
+
+We do a lot of things but only blog about some of them.
+
+Here are some recent selections.
+
+.. postlist:: 10
+   :format: {title} - {author}, {date}
+   :excerpts:
+
+.. toctree::
+   :hidden:
